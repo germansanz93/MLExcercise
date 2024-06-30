6b8f4878e36f9f9ec3a63306da70ef6f5f22c706
German Sanz <germansanz93@gmail.com>
2024-06-30 09:59:41 -0300

    added some graphics for data

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import pandas as pd
 import tarfile
 import urllib.request
+import matplotlib.pyplot as plt
 
 def load_housing_data():
   tarball_path = Path("datasets/housing.tgz")
@@ -13,12 +14,20 @@ def load_housing_data():
       housing_tarball.extractall(path="datasets")
   return pd.read_csv(Path("datasets/housing/housing.csv"))
 
-housing = load_housing_data()
-print("head: ")
-print(housing.head(), "\n")
-print("info: ")
-print(housing.info(), "\n")
-print("ocean_proximity categories: ")
-print(housing["ocean_proximity"].value_counts(), "\n")
-print("describe: ")
-print(housing.describe(), "\n") # Los nulls son ignorados, std es la desviacion estandar
+def print_info(housing):
+  print("head: ")
+  print(housing.head(), "\n")
+  print("info: ")
+  print(housing.info(), "\n")
+  print("ocean_proximity categories: ")
+  print(housing["ocean_proximity"].value_counts(), "\n")
+  print("describe: ")
+  print(housing.describe(), "\n") # Los nulls son ignorados, std es la desviacion estandar
+
+  housing.hist(bins=50, figsize=(12, 8))
+  plt.savefig("plot.png")
+  # plt.show()
+
+if __name__ == '__main__':
+  housing = load_housing_data()
+  print_info(housing)
